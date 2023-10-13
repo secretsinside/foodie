ec8c03a47f838309f916dc6e33856168bfbe7dc4
Rohit Kumar <07nith.me@gmail.com>
2023-10-13 16:36:56 +0530

    added <Link > to header tabs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
@@ -8,9 +9,15 @@ const Header = () => {
             </div>
             <div className='header-menu'>
                 <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About us</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact us</Link>
+                    </li>
                     <li>Cart</li>
                 </ul>
             </div>
